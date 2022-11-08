2e3dbb89546050b95dc6f9dbf9bfab12a36df5f5
khv2644511 <khv2644511@naver.com>
2022-11-08 20:19:11 +0900

    reset클릭 시 계속 타이머 실행

--- 타이머/main.js
+++ 타이머/main.js
@@ -1,27 +1,72 @@
 /** @format */
 
+const $hour = document.querySelector("#hour");
+const $hour_txt = document.querySelector("#hour_txt");
+
+const $min = document.querySelector("#min");
+const $min_txt = document.querySelector("#min_txt");
+
 const $sec = document.querySelector("#sec");
 const $sec_txt = document.querySelector("#sec_txt");
+
 const $start_btn = document.querySelector("#start_btn");
 const $reset_btn = document.querySelector("#reset_btn");
 
 const $start_btn_df = document.querySelector("#start_btn_df");
 const $reset_btn_df = document.querySelector("#reset_btn_df");
 
+$hour.addEventListener("click", addHour);
+$min.addEventListener("click", addMinc);
 $sec.addEventListener("click", addSec);
-$start_btn_df.addEventListener("click", startTimer);
 
-let i = 0;
+$start_btn_df.addEventListener("click", startTimer);
+$reset_btn_df.addEventListener("click", resetTimer);
+
+let hour = 0;
+let min = 0;
+let sec = 0;
+
+function addHour() {
+  console.log("c");
+  if (hour < 60) {
+    hour += 1;
+    $hour_txt.innerText = `${hour}`;
+  } else if (hour >= 60) {
+    hour = 0;
+    $hour_txt.innerText = `${hour}0`;
+  }
+  $start_btn.classList.add("hidden");
+  $reset_btn.classList.add("hidden");
+
+  $start_btn_df.style.display = "block";
+  $reset_btn_df.style.display = "block";
+}
+
+function addMinc() {
+  console.log("c");
+  if (min < 60) {
+    min += 1;
+    $min_txt.innerText = `${min}`;
+  } else if (i >= 60) {
+    min = 0;
+    $min_txt.innerText = `${min}0`;
+  }
+  $start_btn.classList.add("hidden");
+  $reset_btn.classList.add("hidden");
+
+  $start_btn_df.style.display = "block";
+  $reset_btn_df.style.display = "block";
+}
 
 // 초 클릭 시 10초씩 증가
 function addSec() {
   console.log("c");
-  if (i < 60) {
-    i += 10;
-    $sec_txt.innerText = `${i}`;
-  } else if (i >= 60) {
-    i = 0;
-    $sec_txt.innerText = `${i}0`;
+  if (sec < 60) {
+    sec += 10;
+    $sec_txt.innerText = `${sec}`;
+  } else if (sec >= 60) {
+    sec = 0;
+    $sec_txt.innerText = `${sec}0`;
   }
   $start_btn.classList.add("hidden");
   $reset_btn.classList.add("hidden");
@@ -34,20 +79,23 @@ let num = 0;
 
 // start 클릭 시 초 타이머 작동
 function startTimer() {
-  let time = parseInt(i);
-
-  setInterval(() => {
+  let time = parseInt(sec);
+  let setIntervalFunc = setInterval(() => {
     if (time > 1) {
       num++;
-      time = parseInt(i) - num;
+      time = parseInt(sec) - num;
       $sec_txt.innerText = time;
       console.log(time);
-      // if (time == 0) {
-      //   console.log("0이다!!!");
-      //   $sec_txt.innerText = "00";
-      // }
     } else if (time == 1) {
       $sec_txt.innerText = "00";
     }
   }, 1000);
+}
+function stopTimer() {
+  clearInterval(setIntervalFunc);
+}
+
+function resetTimer() {
+  $sec_txt.innerText = "00";
+  clearInterval(setIntervalFunc);
 }
